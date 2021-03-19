7fd32266bbdbdcd0af7b9f0e160461e68ca2f855
damatoaj <mr.arthurdamato@gmail.com>
2021-03-19 11:55:00 -0700

    changed date again

--- pages/components/ExerciseTime.js
+++ pages/components/ExerciseTime.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 export default function ExerciseTime (props) {
     const data = props.data;
     console.log(data)
-    console.log(data.date)
     // console.log(data.length)
     // console.log(data.date)
 
