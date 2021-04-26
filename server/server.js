0f6c5a2a2e03f6cf6e1fef3471adf2588403f54e
damatoaj <mr.arthurdamato@gmail.com>
2021-04-26 16:37:22 -0700

    started making an express backend

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const next = require('next');
+
+const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const db = require('../db/models');
+
+//borrowed custom middleware code from p2 express
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app
+    .prepare()
+    .then(() => {
+        const server = express();
+
+        // server.get("/", (req, res) => {
+        //     return res.render()
+        // })
+
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        });
+
+        server.listen(PORT, err => {
+            if (err) throw err;
+            console.log(`> Ready on ${PORT}`);
+        });
+    })
+    .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
+    });
