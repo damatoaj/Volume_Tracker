7352932e10389c614b75919f02d4c90ebc918665
damatoaj <mr.arthurdamato@gmail.com>
2021-04-28 10:24:40 -0700

    try moving folder

--- db/models/index.js
+++ db/models/index.js
@@ -6,10 +6,8 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/db/config/config.json')[env];
-const config = require(__dirname + '/db/models/config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-console.log(__dirname);
 
 let sequelize;
 if (config.use_env_variable) {
